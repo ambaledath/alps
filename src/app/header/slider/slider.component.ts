40bdfb7a814a14ec122f8505c70cf92331270875
Yagna Sharva <ysharva@localhost.localdomain>
2017-11-14 22:02:28 +0530

    assets and text content updates

--- src/app/header/slider/slider.component.ts
+++ src/app/header/slider/slider.component.ts
@@ -14,12 +14,12 @@ export class SliderComponent implements OnInit {
       var sliderContainer = document.getElementById('slider');
       var sliderCounter = 0;
       setInterval(function() {
-        for (var i = 0; i <= 7; i++) {
+        for (var i = 0; i <= 2; i++) {
           sliderContainer.getElementsByTagName('img')[i].style.opacity = "0";
         }
         sliderContainer.getElementsByTagName('img')[sliderCounter].style.opacity = "1";
         sliderCounter = sliderCounter + 1;
-        if (sliderCounter > 7) {
+        if (sliderCounter > 2) {
           sliderCounter = 0;
         }
       }, 3000);
